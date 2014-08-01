76c7101383ea7f4467d796e0d0365c44b0916639
Piotr Ludwiczuk <piotrosz@gmail.com>
2014-08-01 16:29:33 +0200

    Convention

---
+++ StructureMapTest/StructureMapConventions.Tests/MyConventionTests.cs
@@ -0,0 +1,23 @@
+﻿namespace StructureMapConventions.Tests
+{
+    using Xunit;
+
+    public class MyConventionTests
+    {
+        [Fact]
+        public void gets_insert_command_handler()
+        {
+            var container = ApplicationConfiguration.Init();
+
+            var resultA = container.GetInstance<ICommandHandler<ClassA>>();
+            var resultB = container.GetInstance<ICommandHandler<ClassB>>();
+            var resultC = container.GetInstance<ICommandHandler<ClassC>>();
+            var resultD = container.GetInstance<ICommandHandler<ClassD>>();
+
+            Assert.True(resultA.GetType() == typeof(InsertCommandHandler<ClassA>));
+            Assert.True(resultB.GetType() == typeof(InsertCommandHandler<ClassB>));
+            Assert.True(resultC.GetType() == typeof(InsertCommandHandler<ClassC>));
+            Assert.True(resultD.GetType() == typeof(InsertCommandHandler<ClassD>));
+        }
+    }
+}
---
+++ StructureMapTest/StructureMapConventions/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace StructureMapConventions
+{
+    using System;
+
+    using StructureMap;
+    using StructureMap.Graph;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ApplicationConfiguration.Init();
+        }
+    }
+
+    public static class ApplicationConfiguration
+    {
+        public static Container Init()
+        {
+            return new Container(x => x.Scan(
+                y =>
+                {
+                    y.TheCallingAssembly();
+                    y.Convention<MyConvention>();
+                    //y.LookForRegistries();
+                }));
+        }
+    }
+
+    public abstract class BaseEntity { }
+    public interface ICommandHandler<T> { }
+    public class ClassA : BaseEntity { }
+    public class ClassB : BaseEntity { }
+    public class ClassC : BaseEntity { }
+    public class ClassD : BaseEntity { }
+    public class InsertCommandHandler<T> : ICommandHandler<T> { }
+    public class SpecialInsertDCommandHandler : ICommandHandler<ClassD> { }
+}
